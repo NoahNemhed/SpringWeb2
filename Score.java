56fec31c3d00fff56e1771c9dea6d75cc9dc0346
NoahNemhed <89108122+NoahNemhed@users.noreply.github.com>
2022-03-14 20:50:41 +0100

    Update Score.java

--- Score.java
+++ Score.java
@@ -3,18 +3,34 @@ package com.example.RockPaperScissors;
 public class Score {
 	static int playerScore;
 	static int computerScore;
+	static int games;
+	static int ties;
 	public static int getPlayerScore() {
 		return playerScore;
 	}
-	public static void setPlayerScore(int playerScore1) {
-		playerScore += playerScore1;
+	public static void setPlayerScore(int playerScore) {
+		Score.playerScore = playerScore;
 	}
 	public static int getComputerScore() {
 		return computerScore;
 	}
-	public static void setComputerScore(int computeScore1) {
-		computerScore += computeScore1;
+	public static void setComputerScore(int computerScore) {
+		Score.computerScore = computerScore;
 	}
+	public static int getGames() {
+		return games;
+	}
+	public static void setGames(int games) {
+		Score.games = games;
+	}
+	public static int getTies() {
+		return ties;
+	}
+	public static void setTies(int ties) {
+		Score.ties = ties;
+	}
+	
+	
 	
 	
 
